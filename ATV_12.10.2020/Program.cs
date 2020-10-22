0203a02779c0b43122842d1e2107d0dba87d73e3
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-10-21 23:09:32 -0300

    Add files via upload
    Atividade complementar do feriado dia 12/10/2020

---
+++ ATV_12.10.2020/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATV_EXER1_12._10._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o número de pessoas: ");
+            //Variável para a entrada de usuário
+            int p = int.Parse(Console.ReadLine());
+            
+            //Array com a variável p para o número a ser usado
+            double[] vect = new double[p];
+
+            int n = 1;
+            Console.WriteLine("Declare a altura das pessoas: ");
+
+            //for para definição das alturas
+            for(int i = 0; i < p; i++)
+            {                
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Alturas das pessoas: " + "("+ n +") "+ vect[i]);
+                n++;
+            }
+
+            //double para soma
+            double s = 0.0;
+
+            for(int i = 0; i < p; i++)
+            {
+                s += vect[i];
+            }
+
+            double media = s / vect.Length;
+            Console.WriteLine("Média: " + media.ToString("F2", CultureInfo.InvariantCulture)); ;
+
+            Console.ReadKey();
+        }
+    }
+}
