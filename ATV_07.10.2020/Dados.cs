7a5edaf990f4826aef7f842f05d962a07e9c4f87
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-10-21 23:15:57 -0300

    Add files via upload
    Exercício do banco

---
+++ ATV_07.10.2020/Dados.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ATV_EXER1_07._10._2020
+{
+    class Dados
+    {
+        public int Conta;
+        public string Nome;
+        public double Deposito;
+
+        public Dados(int conta, string nome, double deposito, double saque)
+        {
+            Conta = conta;
+            Nome = nome;
+            Deposito = deposito;
+        }
+
+        public void Depositar(double quant)
+        {
+            Deposito += quant;
+        }
+
+        public void Sacar(double quant)
+        {
+            Deposito -= quant;
+            Deposito -= 5;
+        }
+
+        public override string ToString()
+        {
+            return "N° da conta: " + Conta
+                + " Titular: " + Nome
+                + " Saldo: R$" + Deposito.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ATV_07.10.2020/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATV_EXER1_07._10._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados a = new Dados(0, "Sem Nome", 0.0, 0.0);
+            Console.WriteLine("Entre com o n° da conta: ");
+            a.Conta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o titular da conta: ");
+            a.Nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S') 
+            { 
+                Console.Write("Entre o valor de depósito inicial: ");
+                double dpstInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                a.Deposito += dpstInicial;
+            }
+            else if(resp == 'n' || resp == 'N')
+            {
+                a.Deposito = 0;
+            }
+
+            Console.WriteLine("Dados da conta: " + a);
+
+            Console.WriteLine("Entre com um valor para depósito: ");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Depositar(qtd);
+
+            Console.WriteLine("Dados atualizados: " + a);
+
+            Console.WriteLine("Entre com um valor para saque: ");
+            qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Sacar(qtd);
+
+            Console.WriteLine("Dados atualizados: " + a);
+        }
+    }
+}
