d1f4ae43381072ca7601eab11ac5f194f411def8
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-10-24 15:00:32 -0300

    Add files via upload
    Atividade de conversor de moedas

---
+++ ATV_16.09.2020/Dolar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATV_EXER1_16._09._2020
+{
+    class Dolar
+    {
+        public static double IOF = 6.0;
+
+        public static double CotacaoDolar(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * IOF / 100.0;
+        }
+    }
+}
---
+++ ATV_16.09.2020/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATV_EXER1_16._09._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares irá comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = Dolar.CotacaoDolar(cotacao, quantia);
+            Console.WriteLine("Valor a ser pago: " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
