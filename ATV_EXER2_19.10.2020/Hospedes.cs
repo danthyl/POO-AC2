62a4a6e8c7ba04062df8f5a0995d157e9db9c0c5
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-10-28 22:43:45 -0300

    Add files via upload
    Atividade sobre o veto/hotel

---
+++ ATV_EXER2_19.10.2020/Hospedes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATV_EXER2_19._10._2020
+{
+    class Hospedes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        //Construtor
+        public Hospedes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        //String para aparecer o nome e email nos quartos ocupados
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ ATV_EXER2_19.10.2020/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+namespace ATV_EXER2_19._10._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array com n° de hospedes/quartos
+            Hospedes[] vect = new Hospedes[10];
+
+            Console.WriteLine("Declare a quantidade de hóspedes: ");
+            //Entrada do usuário da quantidade de hospedes
+            int n = int.Parse(Console.ReadLine());            
+
+            //for para declarar nome, email e o quarto
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\nDeclare o nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Declare o e-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Declare o quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                //array com as informações
+                vect[quarto] = new Hospedes(nome, email);
+            }
+
+            Console.WriteLine();
+            //Demonstração dos quartos ocupados
+            Console.WriteLine("Quartos Ocupados: ");
+            for(int i = 0; i < 10; i++)
+            {
+                //demonstrar os quartos ocupados
+                if(vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }    
+}
