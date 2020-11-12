d40dd829456fe5cd0470f8b50866ccf09b4a0399
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-11-11 22:44:00 -0300

    Add files via upload
    Exercício do exemplo da aula 11/11/2020

---
+++ ATV_11.11.2020/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATV_EXER_11._11._2020
+{
+    class Conta
+    {
+        public int Numero { get; private set; } //encapsulamento
+        public string Titular { get; private set; } //encapsulamento
+        public double Saldo { get; protected set; } //Protected para haver alterações entre classes
+
+        //Construtor padrão e outro com paramêtros
+        public Conta() { }
+        public Conta(int numero, string titular, double saldo) 
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        //Métodos para Saque e Déposito
+        public void Saque(double total)
+        {
+            Saldo -= total;
+        }
+
+        public void Deposito(double total)
+        {
+            Saldo += total;
+        }
+
+    }
+}
---
+++ ATV_11.11.2020/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ATV_EXER_11._11._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaJuridica conta = new ContaJuridica(8010, "Lara Croft", 100.0, 500.0);
+
+            Console.Write("Dados da conta: N°" + conta.Numero + ", Titular: " + conta.Titular
+                + ", Saldo: " + conta.Saldo + ", Empréstimo Limite: " + conta.EmprestimoLimite);
+
+            Console.WriteLine("\nDeclare a quantidade para saque: ");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(qtd);
+
+            Console.WriteLine("Dados atualizados da conta: N°" + conta.Numero + ", Titular: " + conta.Titular
+                + ", Saldo: " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", Empréstimo Limite: " + conta.EmprestimoLimite);
+
+            Console.WriteLine("\nDeclare a quantidade para depósito: ");
+            qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(qtd);
+
+            Console.WriteLine("Dados atualizados da conta: N°" + conta.Numero + ", Titular: " + conta.Titular
+                + ", Saldo: " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Empréstimo Limite: " + conta.EmprestimoLimite);
+
+            //Caso o valor total do saldo seja menor que o empréstimo
+            if (conta.Saldo <= conta.EmprestimoLimite)
+            {
+                Console.WriteLine("\nDeclare a quantidade para empréstimo: ");
+                qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Emprestimo(qtd);
+
+                Console.WriteLine("Dados atualizados da conta: N°" + conta.Numero + ", Titular: " + conta.Titular
+                + ", Saldo: " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Empréstimo Limite: " + conta.EmprestimoLimite);
+            }
+            Console.ReadKey();
+        }
+    }
+}
