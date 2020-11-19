1dd4f1b7d21fd2825cb88b2ac1db838d5661fa43
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2020-11-18 23:40:44 -0300

    Add files via upload
    Segundo exercício complementar do dia 02/11

---
+++ ATV_EXER2_02.11.2020/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ATV_EXER2_02._11._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definiçaõ das linhas e colunas
+            Console.WriteLine("\nDeclare a linha e coluna: ");
+            Console.WriteLine("Dê espaço após declarar a linha para declarar a coluna ");
+            string[] line = Console.ReadLine().Split(' ');
+            int m = int.Parse(line[0]);
+            int n = int.Parse(line[1]);
+
+            //Matriz 
+            int[,] mat = new int[m, n];
+
+            //Valores dentro da matriz
+            for (int i = 0; i < m; i++)
+            {
+                string[] value = Console.ReadLine().Split(' ');
+                for (int a = 0; a < n; a++)
+                {
+                    mat[i, a] = int.Parse(value[a]);
+                }
+            }
+
+            //Declaração de um número para encontrar na Matriz
+            Console.WriteLine("\nDeclare um número inteiro que tem na matriz: ");
+            int x = int.Parse(Console.ReadLine());
+
+            //Busca da posição do número na Matriz
+            for (int i = 0; i < m; i++)
+            {
+                for (int a = 0; a < n; a++)
+                {
+                    if (mat[i, a] == x)
+                    {
+                        Console.WriteLine("\nPosicionamento: " + i + ", " + a + ": ");
+                        if (a > 0)
+                        {
+                            //Número posicionado na esquerda
+                            Console.WriteLine("Esquerda: " + mat[i, a - 1]);
+                        }
+                        else if (i > 0)
+                        {
+                            //Número posicionado acima
+                            Console.WriteLine("Acima: " + mat[i - 1, a]);
+                        }
+                        else if (a < n - 1)
+                        {
+                            //Número posicionado na direita
+                            Console.WriteLine("Direita: " + mat[i, a + 1]);
+                        }
+                        else if (a < m - 1)
+                        {
+                            //Número posicionado abaixo
+                            Console.WriteLine("Abaixo: " + mat[i + 1, a]);
+                        }
+                    }
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
